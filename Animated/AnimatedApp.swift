091e8a0608e8e62fdcb99cb4b77f1f3fc0b727a7
DanielSarkwa <dhannysark@outlook.com>
2023-05-31 06:30:13 +0300

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Daniel Yeboah on 31.5.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
